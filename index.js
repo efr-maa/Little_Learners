893cbfecd1c8bcd08e05e4e81f9b4be9595349fc
Elizabeth Frimpomaa <elizabethfrimpomaa887@gmail.com>
2026-07-06 22:35:53 -0400

    index.js add up

--- index.js
+++ index.js
@@ -10,16 +10,17 @@
 ***/
 
 // Step 1: Select the theme button
+let themeButton = document.getElementById("theme-button");
 
 // Step 2: Write the callback function
 const toggleDarkMode = () => {
-    // Write your code here
+    document.body.classList.toggle("dark-mode");
     // This section will run whenever the button is clicked
 }
 
 // Step 3: Register a 'click' event listener for the theme button,
 //             and tell it to use toggleDarkMode as its callback function
-
+themeButton.addEventListener("click", toggleDarkMode);
 
 /*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
 /*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
